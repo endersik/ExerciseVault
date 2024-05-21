8504de855fdab1248fd85deff2d777b2869d0125
Ender Şık <mailtoender@gmail.com>
2024-05-21 09:51:14 +0300

    Add category from script

--- vault.js
+++ vault.js
@@ -4,42 +4,49 @@ const data = [
         name: "Push Up",
         img: "https://www.spotebi.com/wp-content/uploads/2014/10/push-up-exercise-illustration.jpg",
         level: "moderate",
-        muscle: ["chest", "arms", "shoulders"]
+        category: "Chest"
     },
     {
         id:2,
         name: "Burpees",
         img: "https://www.spotebi.com/wp-content/uploads/2014/10/burpees-exercise-illustration.jpg",
         level: "light",
-        muscle: ["legs", "abs"]
+        category: "Legs"
     },
     {
         id:3,
         name: "Plank",
         img: "https://www.spotebi.com/wp-content/uploads/2014/10/plank-exercise-illustration.jpg",
         level: "moderate",
-        muscle: ["abs"]
+        category: "Abs"
     },
     {
         id:4,
         name: "Split Squat",
         img: "https://www.spotebi.com/wp-content/uploads/2015/05/bulgarian-split-squat-exercise-illustration.jpg",
         level: "hard",
-        muscle:["legs"]
+        category:"Legs"
     },
     {
         id:5,
         name: "Arch Up",
         img: "https://i.pinimg.com/736x/36/92/a0/3692a0267ced7d822e81e5bc15f883ca.jpg",
         level: "light",
-        muscle: ["back"]
+        category: "Back"
     },
     {
         id:6,
         name: "Wall Crunch",
         img: "https://i.pinimg.com/736x/24/fe/02/24fe02a7413ed86267aaae7db7607ebd.jpg",
         level: "hard",
-        muscle: ["abs"]
+        category: "Abs"
+    },
+    {
+        id:7,
+        name: "Arnold Shoulder Press",
+        img: "https://i.pinimg.com/736x/fb/94/81/fb948178a1e39822aa7878a11223e4ac.jpg",
+        level: "light",
+        category: "Shoulder"
     }
 ];
 
@@ -76,3 +83,26 @@ searchInput.addEventListener("keyup", (e) => {
     }
     else displayExercises(data);
 })
+
+const setCategories = () => {
+    const allCategories = data.map(item => item.category)
+    // Filter allCategories to keep only unique categories.
+    // This is achieved by checking if the first occurrence of the current 
+    // category (`item`) within the `allCategories` array matches its current 
+    // index (`i`).
+    const categories = [
+        "All",
+        ...allCategories.filter((item, i) => {
+            return allCategories.indexOf(item) === i;
+        })]
+
+    categoriesContainer.innerHTML = categories.map( category => 
+    `
+    <span class="category">${category}</span>
+    `
+).join("");
+};
+
+
+
+setCategories()
